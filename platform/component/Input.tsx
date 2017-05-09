a34b555163dcfd0a27965f85d6c101ff2c1d6688
KostiaSA <savchenkov@buhta.ru>
2017-05-09 21:00:09 +0300

    --

--- platform/component/Input.tsx
+++ platform/component/Input.tsx
@@ -9,6 +9,11 @@ import {IconMixin} from "./mixin/IconMixin";
 import {HeightWidthMixin} from "./mixin/HeightWidthMixin";
 import {CSSProperties, KeyboardEvent, SyntheticEvent} from "react";
 import {DraggableResizable} from "../react/DraggableResizable";
+import {AgGridReact} from "ag-grid-react";
+import {isArray, isBoolean, isNumber, isString} from "util";
+import {AgGridColDef} from "../react/AgGridColDef";
+import GridApi = ag.grid.GridApi;
+import ColumnApi = ag.grid.ColumnApi;
 
 
 export interface IInputStyle {
@@ -23,6 +28,29 @@ export const DefaultInputStyle: IInputStyle = {
     backgroundColor: "white"
 
 };
+
+export type InputValueType = "auto" | "string" | "number" | "boolean";
+export type InputComboType = "none" | "array" | "sql" | "function" | "system";
+
+export interface IComboBoxItem {
+    label?: string;
+    value?: any;
+    checked?: boolean;
+    disabled?: boolean;
+    group?: string;
+    hasThreeStates?: boolean;
+    html?: string;
+    image?: string;
+}
+
+class AgGrid_CellRenderer extends React.Component<any, any> {
+
+    render() {
+        let style: CSSProperties = {};
+        return <span style={style}>{this.props.value}</span>;
+    }
+}
+
 
 export class Input extends EnabledMixin(
     TopLeftMixin(
@@ -104,6 +132,131 @@ export class Input extends EnabledMixin(
     //     return pe;
     // }
 
+
+    get actualValueType(): InputValueType {
+        if (this.valueType === "auto") {
+            if (this.bindObject && this.bindObject[this.bindProperty] !== undefined) {
+                let value = this.bindObject[this.bindProperty];
+                if (isString(value))
+                    return "string";
+                else if (isNumber(value))
+                    return "number";
+                else if (isBoolean(value))
+                    return "boolean";
+                else
+                    throw "Input.actualValueType(): неизвестный тип переменной '" + this.bindProperty + "'";
+            }
+            else
+                return "string";
+        }
+        else
+            return this.valueType;
+    }
+
+    // ------------------------------ valueType ------------------------------
+    _valueType: InputValueType = "auto";
+    get valueType(): InputValueType {
+        return this._valueType;
+    }
+
+    set valueType(value: InputValueType) {
+        let needReloadPropertyEditor = this._valueType !== value;
+        this._valueType = value;
+        // if (this.$ && needReloadPropertyEditor && this._designer) {
+        //     this._designer.reloadPropertyEditor();
+        // }
+    }
+
+    // private __emitCode_valueType(code: EmittedCode) {
+    //     code.emitStringValue(this, "valueType", "auto");
+    // }
+    //
+    // private __getPropertyEditor_valueType(): PropertyEditor {
+    //     let pe = new StringPropertyEditor();
+    //     pe.propertyName = "valueType";
+    //     pe.comboType = "array";
+    //     pe.comboItemsArray = ["auto", "string", "number", "boolean"];
+    //     pe.category = Категория_ПривязкаДанных;
+    //     return pe;
+    // }
+
+    // ------------------------------ comboType ------------------------------
+    _comboType: InputComboType = "none";
+    get comboType(): InputComboType {
+        return this._comboType;
+    }
+
+    set comboType(value: InputComboType) {
+        let needReloadPropertyEditor = this._comboType !== value;
+        this._comboType = value;
+
+        // if (this.$ && needReloadPropertyEditor) {
+        //     if (this.comboType === "array")
+        //         this.comboItemsArray = this._comboItemsArray;
+        // }
+        //
+        // if (this.$ && needReloadPropertyEditor && this._designer) {
+        //     this._designer.reloadPropertyEditor();
+        // }
+    }
+
+    // private __emitCode_comboType(code: EmittedCode) {
+    //     code.emitStringValue(this, "comboType", "none");
+    // }
+    //
+    // private __getPropertyEditor_comboType(): PropertyEditor {
+    //     let pe = new StringPropertyEditor();
+    //     pe.comboType = "array";
+    //     pe.comboItemsArray = ["none", "array", "sql", "function", "system"];
+    //     pe.propertyName = "comboType";
+    //     pe.category = Категория_ComboBox;
+    //     return pe;
+    // }
+
+    // ------------------------------ comboItemsArray ------------------------------
+    _comboItemsArray: any[];
+    get comboItemsArray(): any[] {
+        return this._comboItemsArray;
+    }
+
+    set comboItemsArray(value: any[]) {
+        this._comboItemsArray = value;
+        if (this.comboType === "array" && isArray(value)) {
+            let comboSource = [];
+            for (let item of value) {
+                if (isString(item)) {   // массив строк
+                    comboSource.push(item);
+                }
+                else if (isArray(item)) {   // [35,"ООО Удача"] - value,label
+                    let comboItem: IComboBoxItem = {
+                        value: item[0],
+                        label: item[1],
+                    };
+                    comboSource.push(comboItem);
+                }
+                else
+                    comboSource.push(item);  // объект в формате IComboBoxItem
+            }
+            //this.jqxWidget({source: comboSource} as jqwidgets.ComboBoxOptions);
+        }
+    }
+
+    // private __emitCode_comboItemsArray(code: EmittedCode) {
+    //     code.emitStringValue(this, "comboItemsArray", "none");
+    // }
+
+    private __setOptions_comboItemsArray() {
+        this.comboItemsArray = this._comboItemsArray;
+    }
+
+    // private __getPropertyEditor_comboItemsArray(): PropertyEditor {
+    //     let pe = new StringPropertyEditor();
+    //     pe.propertyName = "comboItemsArray";
+    //     pe.category = Категория_ПривязкаДанных;
+    //     return pe;
+    // }
+
+
     // ------------------------------ getReactElement ------------------------------
 
     internalValue: string;
@@ -150,6 +303,34 @@ export class Input extends EnabledMixin(
             });
         }
     }
+
+
+    getColumnDefs(): ag.grid.ColDef[] {
+        let cols: AgGridColDef[] = [];
+
+        let fromCol: AgGridColDef = {
+            headerName: "от кого",
+            field: "отправитель",
+            maxWidth: 300,
+            cellStyle: {whiteSpace: "normal"},
+            cellRendererFramework: AgGrid_CellRenderer,
+        };
+        cols.push(fromCol);
+
+        return cols;
+    }
+
+    comboGridApi: GridApi;
+    comboGridColumnApi: ColumnApi;
+    comboGridRowData: any[] = [];
+
+
+    gridReadyHandler = (event: { api: GridApi, columnApi: ColumnApi }) => {
+        this.comboGridApi = event.api;
+        this.comboGridColumnApi = event.columnApi;
+        //console.log("gridReadyHandler", event);
+        //event.comboGridApi.sizeColumnsToFit();
+    };
 
 
     getReactElement(index?: number | string): JSX.Element | null {
@@ -218,7 +399,25 @@ export class Input extends EnabledMixin(
                     }}
                     style={popupStyle}
                 >
-                    это попап
+                    <AgGridReact
+                        rowHeight={58}
+                        columnDefs={this.getColumnDefs()}
+                        suppressColumnVirtualisation={true}
+                        enableSorting={true}
+                        enableFilter={false}
+                        rowData={this.comboGridRowData}
+                        onGridReady={this.gridReadyHandler}
+                        rowStyle={{cursor: "pointer"}}
+                        onGridSizeChanged={() => {
+                            if (this.comboGridApi) this.comboGridApi.sizeColumnsToFit()
+                        }}
+                        onRowDataChanged={() => {
+                            if (this.comboGridApi) this.comboGridApi.sizeColumnsToFit()
+                        }}
+                        onRowClicked={(event: any) => {
+                            //appState.openIncomeMessagePage(event.data.документКлюч,this.props.navigatorBranch)
+                        }}
+                    />
                 </DraggableResizable>
             </span>
         );
